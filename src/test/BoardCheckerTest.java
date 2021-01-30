aa33985528de614f6a104d2c50999af762aa273a
StijnOdink28 <s.odink@student.utwente.nl>
2021-01-30 12:56:09 +0100

    Fixed tests

--- src/test/BoardCheckerTest.java
+++ src/test/BoardCheckerTest.java
@@ -7,6 +7,9 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static model.game.Board.HEIGHT;
+import static model.game.Board.WIDTH;
+
 public class BoardCheckerTest {
     private Board board;
     @BeforeEach
@@ -18,7 +21,7 @@ public class BoardCheckerTest {
     public void checkValidBoard () {
         Assertions.assertTrue(board.checkValidBoard());
         Board board1 = board;
-        for (int i = 0; i < 150; i ++) {
+        for (int i = 0; i < (WIDTH * HEIGHT); i ++) {
             if (board1.getField(i).getShip().getType().equals(ProtocolMessages.Ship.CARRIER)) {
                 board1.getFieldsArray()[i] = new Field();
                 break;
@@ -26,27 +29,28 @@ public class BoardCheckerTest {
         }
         Assertions.assertFalse(board1.checkValidBoard());
         Board board2 = board;
-        for (int i = 0; i < 150; i ++) {
+        for (int i = 0; i < (WIDTH * HEIGHT); i ++) {
             if (board2.getField(i).getShip().getType().equals(ProtocolMessages.Ship.DESTROYER)) {
                 board2.getFieldsArray()[i] = new Field();
             }
         }
         Assertions.assertFalse(board2.checkValidBoard());
         Board board3 = board;
-        for (int i = 135; i < 150; i ++) {
+        for (int i = (WIDTH * (HEIGHT - 1)); i < (WIDTH * HEIGHT); i ++) {
             if (!board3.getField(i).getShip().getType().equals(ProtocolMessages.Ship.EMPTY)) {
                 board3.getFieldsArray()[i] = new Field();
+                break;
             }
         }
         Assertions.assertFalse(board3.checkValidBoard());
         Board board4 = board;
-        for (int i = 0; i < 150; i ++) {
+        for (int i = 0; i < (WIDTH * HEIGHT); i ++) {
             if (board4.getField(i).getShip().toString().equals("SUPERPATROL1")) {
                 board4.getFieldsArray()[1] = new Field();
             }
         }
         Ship ship = new Ship(1, ProtocolMessages.Ship.SUPERPATROL);
-        for (int i = 14; i < 135; i = i + 15) {
+        for (int i = (WIDTH - 1); i < (WIDTH * (HEIGHT - 1)); i = i + WIDTH) {
             if (board4.getField(i).getShip().getType().equals(ProtocolMessages.Ship.EMPTY) && board4.getField(i + 1).getShip().getType().equals(ProtocolMessages.Ship.EMPTY)) {
                 board4.getFieldsArray()[i] = new Field(ship);
                 board4.getFieldsArray()[i + 1] = new Field(ship);
--- src/test/BoardMakerTest.java
+++ src/test/BoardMakerTest.java
@@ -7,6 +7,9 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static model.game.Board.HEIGHT;
+import static model.game.Board.WIDTH;
+
 public class BoardMakerTest {
     private Board board;
     @BeforeEach
@@ -18,19 +21,19 @@ public class BoardMakerTest {
     public void checkEnoughShips () {
         int correctShipCounter = 0;
         for (Ship s : board.SHIPS) {
-            for (int i = 0; i < 150; i ++) {
+            for (int i = 0; i < (WIDTH * HEIGHT ); i ++) {
                 if (board.getField(i).getShip().equals(s)) {
                     correctShipCounter ++;
                     break;
                 }
             }
         }
-        Assertions.assertEquals(correctShipCounter, 28);
+        Assertions.assertEquals(correctShipCounter, board.SHIPS.length);
     }
 
     @Test
     public void checkNoShipsOverBoundary() {
-        for (int i = 14; i < 135; i = i + 15) {
+        for (int i = (WIDTH - 1); i < (WIDTH * (HEIGHT - 1)); i = i + WIDTH) {
             if (!board.getField(i).getShip().getType().equals(ProtocolMessages.Ship.EMPTY) && !board.getField(i + 1).getShip().getType().equals(ProtocolMessages.Ship.EMPTY)) {
                 Assertions.assertNotEquals(board.getField(i).getShip(), board.getField(i + 1).getShip());
             }
